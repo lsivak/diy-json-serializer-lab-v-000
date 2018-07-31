7b1ab5b1d9f07958fa57415c6c6db165f382d0da
Lara Sanders <larasanders@gmail.com>
2018-07-31 22:56:54 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,5 @@
 class ProductsController < ApplicationController
 
-    def data
-      product = Product.find(params[:id])
-      render json: ProductSerializer.serialize(product)
-    end
-
   def index
     @products = Product.all
   end
@@ -26,6 +21,10 @@ class ProductsController < ApplicationController
   def create
     Product.create(product_params)
     redirect_to products_path
+  end
+
+  def show
+    @product = Product.find(params[:id])
   end
 
   private
